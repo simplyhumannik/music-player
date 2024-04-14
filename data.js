c22a09ad450ff11e1ea70e4e856af3a40e8fb12b
nikunj <nikunjsinghal09032004@gmail.com>
2024-04-15 01:17:01 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,50 @@
+let songs = [
+    {
+        name: 'Bounce Baby',
+        path: 'assets/songs/Song 1.mp3',
+        artist: 'RACX-RECORDS',
+        cover: 'assets/images/cover 1.jpg'
+    },
+    {
+        name: 'Kiska Hua',
+        path: 'assets/songs/song 2.mp3',
+        artist: 'RACX-RECORDS',
+        cover: 'assets/images/cover 2.jpg'
+    },
+    {
+        name: 'After You',
+        path: 'assets/songs/song 3.mp3',
+        artist: 'RACX-RECORDS',
+        cover: 'assets/images/cover 3.jpg'
+    },
+    {
+        name: 'Sapna',
+        path: 'assets/songs/song 4.mp3',
+        artist: 'RACX-RECORDS',
+        cover: 'assets/images/cover 4.jpg'
+    },
+    {
+        name: 'Yamraj',
+        path: 'assets/songs/song 5.mp3',
+        artist: 'RACX-RECORDS',
+        cover: 'assets/images/cover 5.jpg'
+    },
+    {
+        name: 'Alwidaa',
+        path: 'assets/songs/song 6.mp3',
+        artist: 'RACX-RECORDS',
+        cover: 'assets/images/cover 6.jpg'
+    },
+    {
+        name: 'Mercy',
+        path: 'assets/songs/song 7.mp3',
+        artist: 'RACX-RECORDS',
+        cover: 'assets/images/cover 7.jpg'
+    },
+    {
+        name: 'Vaastey',
+        path: 'assets/songs/song 8.mp3',
+        artist: 'RACX-RECORDS',
+        cover: 'assets/images/cover 8.jpg'
+    }
+]
